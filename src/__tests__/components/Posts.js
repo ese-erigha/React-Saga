2aa39fba4b94ab4f694d7000c47e0bcecf8ca9e7
Eseoghene Erigha <ese.erigha@gmail.com>
2018-12-24 15:54:23 +0100

    Added redux connected component test

--- src/__tests__/components/Posts.js
+++ src/__tests__/components/Posts.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import {Posts} from '../../modules/posts/containers/Posts';
-import {shallow} from 'enzyme';
+import ConnectedPosts,{Posts} from '../../modules/posts/containers/Posts';
+import {shallow, mount} from 'enzyme';
 import ContentHeader from '../../shared/components/ContentHeader';
+import configureStore from 'redux-mock-store';
+import {Provider} from 'react-redux';
+import '../../setupTests';
 
-describe('Posts Component',()=>{
+
+
+describe('The Posts Component - shallow rendering component',()=>{
 
     let wrapper;
     let dummyPosts = [{id: 40, title: 'Hello world', body: 'It is good to learn react.js'}];
@@ -44,4 +49,51 @@ describe('Posts Component',()=>{
         wrapper.instance().componentDidMount();
         expect(mockFetchPosts.mock.calls.length).toBe(3);
     });
+});
+
+
+describe("The Posts Component - Shallow rendering + mocking and passing store",()=>{
+
+    const initialState = {
+        loading: false,
+        users:[],
+        posts:[],
+        error: {}
+    };
+
+    const mockFetchPosts = jest.fn(()=> dummyPosts);
+
+    let props = {
+
+        fetchPosts: mockFetchPosts,
+        posts: [],
+        loading: false,
+        error: {message: ""},
+        match: {
+            params: {
+                userId: 5
+            }
+        }
+    };
+
+    const mockStore = configureStore();
+    let store,wrapper;
+
+    beforeEach(()=>{
+
+        store = mockStore(initialState);
+        wrapper = shallow(<ConnectedPosts {...props}/>,{context: {store}}); //https://github.com/airbnb/enzyme/issues/1002#issuecomment-421535999
+        
+    });
+
+    it('should render the smart component', () => {
+        
+        expect(wrapper.length).toEqual(1);
+    });
+
+    //https://github.com/airbnb/enzyme/issues/384#issuecomment-219156593
+    //https://github.com/airbnb/enzyme/issues/384#issuecomment-356909808
+    it('+++ check Prop matches with initialState', () => {
+        expect(wrapper.instance().props.loading).toEqual(initialState.loading); 
+     });
 });
--- src/modules/users/usersActions.js
+++ src/modules/users/usersActions.js
@@ -5,8 +5,7 @@ export const FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS';
 export const FETCH_USERS_ERROR = 'FETCH_USERS_ERROR';
 
 
-
 //Users Actions
-export const fetchUsersAction = () => ({ type: FETCH_USERS});
+export const fetchUsersAction = () => ({ type: FETCH_USERS });
 export const fetchUsersSuccessAction = payload => ({ type: FETCH_USERS_SUCCESS, payload });
 export const fetchUsersErrorAction = payload => ({ type: FETCH_USERS_ERROR, payload });
